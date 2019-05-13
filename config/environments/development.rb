273f13364b8adb57cf2f3ef5d5558c2190d229fb
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-05-13 21:48:01 +0500

    username and password is hidden in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: Rails.application.credentials.action_mailer_user_name,  # не используйте для тестов свои реальные ящики
-    password: Rails.application.credentials.action_mailer_password,  #'',
+    user_name: Rails.application.credentials.development[:action_mailer_user_name],
+    password: Rails.application.credentials.development[:action_mailer_password],
     authentication: 'plain',
     enable_starttls_auto: true
   }
