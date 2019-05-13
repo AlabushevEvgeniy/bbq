af902412c9b821f3dfd053a8f55fbfa88f11137f
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-05-13 14:34:30 +0500

    add reset_password_instructions.html.erb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: 'gudprogtest', # не используйте для тестов свои реальные ящики
-    password: '',
+    user_name: Rails.application.credentials.action_mailer_user_name,  # не используйте для тестов свои реальные ящики
+    password: Rails.application.credentials.action_mailer_password,  #'',
     authentication: 'plain',
     enable_starttls_auto: true
   }
