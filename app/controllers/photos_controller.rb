b8801fd647ee4ba570bcce2033f3b126c0c75cc7
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-05-15 21:32:50 +0500

    empty commit beacause of fignya with git

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,23 +1,22 @@
 class PhotosController < ApplicationController
   before_action :set_event, only: [:create, :destroy]
   before_action :set_photo, only: [:destroy]
+  before_action :user_signed_in?, only: [:create]
 
-  # Обратите внимание: фотку может сейчас добавить даже неавторизованный пользовать
-  # Смотрите домашки!
   def create
     # Создаем новую фотографию у нужного события @event
     @new_photo = @event.photos.build(photo_params)
     # Проставляем у фотографии пользователя
     @new_photo.user = current_user
 
-    if user_signed_in? && @new_photo.save
+    if @new_photo.save
       #Уведомляем подписчиков
       notify_subscribers(@event, @new_photo)
       # Если фотка сохранилась, редиректим на событие с сообщением
       redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
       # Если нет — рендерим событие с ошибкой
-      render 'events/show', alert: I18n.t('controllers.photos.error')
+      redirect_to @event, alert: I18n.t('controllers.photos.error')
     end
   end
 
