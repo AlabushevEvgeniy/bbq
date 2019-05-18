fc83455fda56ba634a002c775b180fe247f738d4
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-05-18 23:34:02 +0500

    comment's author don't receive a notification about the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -49,7 +49,7 @@ class CommentsController < ApplicationController
     def notify_subscribers(event, comment)
       # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
       all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-
+      all_emails -= [current_user.email] if current_user
       # По адресам из этого массива делаем рассылку
       # Как и в подписках, берём EventMailer и его метод comment с параметрами
       # И отсылаем в том же потоке
