d95cd5aeb6149c0d2b28b8d2df43c65b43b24b03
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-06-07 23:31:16 +0500

    success event_policy test when user is not an owner

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 RSpec.describe EventPolicy do
 
-  subject {EventPolicy.new(user, event)}
+  subject { EventPolicy.new(user, event) }
+  let(:event) { FactoryBot.create(:event, user: FactoryBot.create(:user)) }
 
   context 'when user is not an owner' do
-    let(:user) {FactoryBot.create(:user)}
-    let(:event) {FactoryBot.create(:event, user: FactoryBot.create(:user))}
+    let(:user) { FactoryBot.create(:user) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.not_to permit_action(:edit) }
+    it { is_expected.not_to permit_action(:update) }
+    it { is_expected.not_to permit_action(:destroy) }
+  end
 
-    it { should permit(:show)}
+  context 'when user is an owner' do
+    let(:user) { event.user }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
   end
 
   # let(:user) {FactoryBot.create(:user)}
