efbf8ac9620f6d10ced49e25068704fda29024ad
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-06-07 18:48:36 +0500

    some unsuccessfull changings

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -1,31 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe EventPolicy do
-  let(:user) {FactoryBot.create(:user)}
-  let(:owner) {FactoryBot.create(:user)}
 
-  subject { EventPolicy }
+  subject {EventPolicy.new(user, event)}
 
   context 'when user is not an owner' do
-    let(:event) {owner.events.build}
+    let(:user) {FactoryBot.create(:user)}
+    let(:event) {FactoryBot.create(:event, user: FactoryBot.create(:user))}
 
-    permissions :update?, :edit?, :destroy? do
-      it { is_expected.not_to permit(user, event) }
-      it { is_expected.not_to permit(nil, event) }
-    end
-
-    permissions :show? do
-      it { should permit(user, event, :show?) }
-    end
+    it { should permit(:show)}
   end
 
-  context 'when user is an owner' do
-    let(:event) {FactoryBot.create(:event, user: owner)}
+  # let(:user) {FactoryBot.create(:user)}
+  # let(:owner) {FactoryBot.create(:user)}
 
-    permissions :update?, :edit?, :destroy? do
-      it { is_expected.to permit(owner, event) }
-    end
-  end
+  # subject { EventPolicy }
+
+  # context 'when user is not an owner' do
+  #   let(:event) {owner.events.build}
+
+  #   permissions :update?, :edit?, :destroy? do
+  #     it { is_expected.not_to permit(user, event) }
+  #     it { is_expected.not_to permit(nil, event) }
+  #   end
+
+  #   permissions :show? do
+  #     it { should permit(user, event, :show?) }
+  #   end
+  # end
+
+  # context 'when user is an owner' do
+  #   let(:event) {FactoryBot.create(:event, user: owner)}
+
+  #   permissions :update?, :edit?, :destroy? do
+  #     it { is_expected.to permit(owner, event) }
+  #   end
+  # end
 
   # context 'being is not an owner' do
   #   let(:user) { User.create }
--- spec/support/matchers/policy_matcher.rb
+++ spec/support/matchers/policy_matcher.rb
@@ -1,13 +1,24 @@
-RSpec::Matchers.define :permit do |user, record, action|
+RSpec::Matchers.define :permit do |action|
   match do |policy|
-    policy.new(user, record).public_send(action)
+    policy.public_send("#{action}?")
   end
 
   failure_message_for_should do |policy|
-    "Expected #{ policy.to_s } to permit #{ action.to_s } on #{ record.to_s } but it didn't."
+    "#{policy.class} does not permit #{action} on #{policy.record} for #{policy.user.inspect}."
   end
 
   failure_message_for_should_not do |policy|
-    "Expected #{ policy.to_s } to forbid #{ action.to_s } on #{ record.to_s } but it didn't."
+    "#{policy.class} does not forbid #{action} on #{policy.record} for #{policy.user.inspect}."
   end
+  # match do |policy|
+  #   policy.new(user, record).public_send(action)
+  # end
+
+  # failure_message_for_should do |policy|
+  #   "Expected #{ policy.to_s } to permit #{ action.to_s } on #{ record.to_s } but it didn't."
+  # end
+
+  # failure_message_for_should_not do |policy|
+  #   "Expected #{ policy.to_s } to forbid #{ action.to_s } on #{ record.to_s } but it didn't."
+  # end
 end
